56c0ef028d5da000d971fcbf46c961bd7537f5d9
Steffany Paiva <76757826+Steffany7406@users.noreply.github.com>
2025-05-10 19:55:31 -0300

    Create Hello_World.cpp

---
+++ Hello_World.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!"; 
+  //instrução de abreviação do character out put que instrui o terminal a exibir uma mensagem.
+}
+//Saída = Hello World!
