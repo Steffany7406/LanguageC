c8f8c4df2cfdad0023b6693a39333c8ebd037cca
Steffany Paiva <76757826+Steffany7406@users.noreply.github.com>
2025-05-10 19:59:20 -0300

    Update Hello_World.cpp

--- Hello_World.cpp
+++ Hello_World.cpp
@@ -1,7 +1,9 @@
-#include <iostream>
+#include <iostream> //Inclui bibliotecas que podemos exibir e usar
 
-int main() {
+int main() { //função para execução de código
   std::cout << "Hello World!"; 
   //instrução de abreviação do character out put que instrui o terminal a exibir uma mensagem.
-}
+} 
 //Saída = Hello World!
+
+//Dica -> C++ é executado de cima para baixo linha por linha.
