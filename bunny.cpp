c7615310255a75fb6a7df7769d8c53b2a7d7a1e9
Steffany Paiva <76757826+Steffany7406@users.noreply.github.com>
2025-05-10 20:58:18 -0300

    Create bunny.cpp

---
+++ bunny.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    std::cout << "_____________________\n";
+    std::cout << "|                   |\n";
+    std::cout << "|     Beautiful     |\n";
+    std::cout << "|       World       |\n";
+    std::cout << "|                   |\n";
+    std::cout << "|___________________|\n";
+    std::cout << "(\__/) ||\n";
+    std::cout << "(o o) ||\n"; // Corrigido para usar 'o' em vez de <U+00B0>
+    std::cout << " \\_   _/ \n"; // Corrigido a barra invertida e o espaço
+    std::cout << "   | |   \n"; // Adicionando uma linha para completar o coelho
+    std::cout << "   |_|   \n"; // Adicionando uma linha para completar o coelho
+    return 0;
+}
