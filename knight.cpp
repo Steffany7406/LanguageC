6cbbbc7bd25c90541ad29bcbf5c41b28802812f7
Steffany Paiva <76757826+Steffany7406@users.noreply.github.com>
2025-05-11 16:12:19 -0300

    Create knight.cpp

---
+++ knight.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+  std::string name = "Brianne";
+
+  std::cout << "Em nome do Guerreiro, eu o conjuro a ser corajoso.\n";
+  std::cout << "Em nome do Pai, eu o conjuro a ser justo.\n";
+  std::cout << "Em nome da Mãe, eu o conjuro a defender os inocentes.\n";
+  std::cout << "Levante-se, "<< name << ", um cavaleiro dos Sete Reinos";
+}
