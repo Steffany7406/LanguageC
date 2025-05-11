88f12dcaf5410d3636dce855615df36a9520190a
Steffany Paiva <76757826+Steffany7406@users.noreply.github.com>
2025-05-11 15:37:42 -0300

    Create temperature.cpp

---
+++ temperature.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+  double fahrenheint = 75.2;
+  double celsius = (fahrenheint - 32) * 5.0 / 9.0;
+
+  std::cout << "A temperatura do Brooklin em fahrenheint equivale a "<< celsius << " graus celsius.";
+  
+}
+// Saída = A temperatura do Brooklin em fahrenheint equivale a 24 graus celsius
